3d24347d25a09663c7fcee375185ba41f91dd734
MatthewDodi <matthew.dodi@gmail.com>
2018-10-29 21:05:28 -0700

    type: feat
    * Added Home.js and Navbar.js components
    * Added react-material

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../App.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import Navbar from './Navbar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#B3E5FC',
+      main: '#03A9F4',
+      dark: '#0288D1',
+      contrastText: '#FFFFFF'
+    },
+    secondary: {
+      light: '#FF9800',
+      main: '#FF9800',
+      dark: '#F57C00',
+      contrastText: '#FFFFFF'
+    },
+    text: '#FFFFFF',
+  },
+  typography: {
+    fontFamily: 'roboto',
+    fontSize: 12,
+    htmlFontSize: 10
+  },
+  overrides: {
+    MuiButton: {
+      raisedPrimary: {
+        textColor: 'white',
+      },
+    },
+  }
+})
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <div className="App">
+            <Navbar />
+          </div>
+        </MuiThemeProvider>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <AppBar position='static'>
+        <Toolbar >
+          <Typography>
+            <Link className='navLink' to ='/'>
+              Home
+            </Link>
+          </Typography>
+          <Typography>
+            <Link className='navLink' to ='/add'>
+              New Question
+            </Link>
+          </Typography>
+          <Typography>
+            <Link className='navLink' to ='/leaderboard'>
+              Leaderboard
+            </Link>
+          </Typography>
+          <Typography style={{marginLeft: 'auto', paddingRight: '2rem'}}>
+            Welcome Matthew
+          </Typography>
+          <Button color='secondary' variant='raised' style={{textTransform: 'none'}}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
+
+export default Navbar;
